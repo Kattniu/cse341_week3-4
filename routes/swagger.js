2f30f299f5e62c17587c3b85a1ad8b03810b07b0
Kattniu <kathe.adriana24@gmail.com>
2025-09-29 20:41:11 +0200

    update

--- routes/swagger.js
+++ routes/swagger.js
@@ -4,6 +4,15 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../swagger.json');  // Importar el swagger.json generado
 
 // Rutas para mostrar la documentación
-router.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+router.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument, {
+  swaggerOptions: {
+    withCredentials: true
+  }
+}));
+
 
 module.exports = router;
+
+//Esto le dice a Swagger UI que:
+//Cuando haga peticiones desde la interfaz (como POST /books), debe enviar automáticamente las cookies, como connect.sid (que contiene la sesión).
+//Esto mejora la compatibilidad para Swagger UI en producción y me asegura que la autenticación con sesiones funcione bien siempre.
